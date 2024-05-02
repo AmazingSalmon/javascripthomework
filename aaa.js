f9dcbb7f10a929190c3a6f51aa438be766496d93
Amazing Salmon <C:\Users\User\AppData\Roaming\The Bat!>
2024-05-02 18:48:48 +0300

    2

--- aaa.js
+++ aaa.js
@@ -1,24 +1,41 @@
-let adminPassword = "master";
-begin = function (pass) {
-    whoami = prompt("who are you?");
-    if(whoami === "admin")
+//****** 1 ******
+//1
+let user = {};
+//2
+user.name = "Mark";
+//3
+user.surname = "Smith";
+//4
+user.name = "Taras";
+//5
+delete user.name;
+//****** 2 ******
+function isEmpty(aObject)
+{
+    return Object.keys(aObject).length === 0;
+}
+
+console.log();
+//****** 3 ******
+let salaries = {
+    Jarik: 1000,
+    Anna: 1600,
+    Miko: 1300
+}
+let sum = 0;
+function getSum(obj) {
+    let sum = 0;
+    for(const key in obj)
     {
-      password = prompt("password: ");
-      if(password === pass)
-      {
-          alert("welcome");
-      } else if (password === null || password === "")
-      {
-          alert("cancelled");
-      } else
-      {
-          alert("wrong password");
-      }
-    } else if (whoami === null || whoami === "")
+        if(typeof(obj[key])==="number") sum += obj[key];
+    }
+    return sum;
+}
+sum = getSum(salaries);
+//****** 4 ******
+function multiplyNumeric(obj) {
+    for(const key in obj)
     {
-        alert("cancelled");
-    } else {
-        alert("I don\'t know you");
+        if(typeof(obj[key])==="number") obj[key] = obj[key]*2;
     }
 }
-begin(adminPassword);
